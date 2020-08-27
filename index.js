b8e1d3a9e9186dd1fa068e5af9a2baff08910b42
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-27 00:13:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
-let formData = {
-  name: name,
-  email: email
-};
- 
-let configObj = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(formData)
-};
- 
-fetch("http://localhost:3000/dogs", configObj)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(object) {
-    console.log(object);
-  });
-  
-  
+
 function submitData() {
     let formData = {
     name: name,
