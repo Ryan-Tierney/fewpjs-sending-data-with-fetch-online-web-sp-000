1474f368b9702e1cc0470715b1e2aa3afdfb7429
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-27 00:13:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,15 +22,27 @@ fetch("http://localhost:3000/dogs", configObj)
   
   
   function submitData() {
-    
+    let formData = {
+    name: name,
+    email: email
+  };
+ 
+    let configObj = {
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+    body: JSON.stringify(formData)
+  };
     fetch("http://localhost:3000/users", configObj) 
     .then(function(response) {
       return response.json(); 
     })
     .then(function(object) {
-      let h2 = document.createElement(h2)
-      h2.innerHTML = object.id;
-      document.body.appendchild(h2);
-      console.log(object);
+    let h2 = document.createElement(h2)
+    h2.innerHTML = object.id;
+    document.body.appendchild(h2);
+    console.log(object);
     })
   }
